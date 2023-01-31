a8c0803eeff97971444f4f71a77c01d460645d0c
AliHMohammad <alihmohammad@hotmail.com>
2023-01-31 13:29:24 +0100

    5545646

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-document.getElementById("OKKnap").addEventListener("click", sigHej);
+document.querySelector("#OKKnap").addEventListener("click", sigHej);
 
 function sigHej() {
-  if (!document.getElementById("navn").value) {
-    document.getElementById("resultat").textContent = "Navn er tomt";
-  } else if (!document.getElementById("alder").value) {
-    document.getElementById("resultat").textContent = "Alder er tomt";   
-  } else if (!document.getElementById("email").value) {
-    document.getElementById("resultat").textContent = "Email er tomt"; 
+  let udfyldt = false;
+  if (!document.querySelector("#navn").value) {
+    document.querySelector("#resultat").textContent = "Navn er tomt";
+  } else if (!document.querySelector("#alder").value) {
+    document.querySelector("#resultat").textContent = "Alder er tomt";   
+  } else if (!document.querySelector("#email").value) {
+    document.querySelector("#resultat").textContent = "Email er tomt"; 
   } else {
-    document.getElementById("resultat").textContent = `Hej ${document.getElementById("navn").value}! Du er ${document.getElementById("alder").value} gammel, og din email er ${document.getElementById("email").value}`; 
+    document.querySelector("#resultat").textContent = `Hej ${document.querySelector("#navn").value}! Du er ${document.querySelector("#alder").value} gammel, og din E-mail er ${document.querySelector("#email").value}`;
+    udfyldt = true;
+  }
+
+  if (document.querySelector("#alder").value >= 30 && udfyldt) {
+    document.querySelector("#uAreOld").textContent = "You are very old!"
+  } else {
+    document.querySelector("#uAreOld").textContent = null;
   }
 }
