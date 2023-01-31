0e20771c3d17aad3115513aab65a22541f12da7d
AliHMohammad <alihmohammad@hotmail.com>
2023-01-31 12:50:37 +0100

    545345

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-document.getElementById("OKKnap").addEventListener("click", sayHello);
+document.getElementById("OKKnap").addEventListener("click", sigHej);
 
-function sayHello() {
-  if (document.getElementById("navn").value) {
-    document.getElementById("resultat").textContent = `Hej ${document.getElementById("navn").value}!`;
+function sigHej() {
+  if (!document.getElementById("navn").value) {
+    document.getElementById("resultat").textContent = "Navn er tomt";
+  } else if (!document.getElementById("alder").value) {
+    document.getElementById("resultat").textContent = "Alder er tomt";   
+  } else if (!document.getElementById("email").value) {
+    document.getElementById("resultat").textContent = "Email er tomt"; 
   } else {
-    document.getElementById("resultat").textContent = "Skriv venligst dit navn. Prøv igen";
+    document.getElementById("resultat").textContent = `Hej ${document.getElementById("navn").value}! Du er ${document.getElementById("alder").value} gammel, og din email er ${document.getElementById("email").value}`; 
   }
 }
