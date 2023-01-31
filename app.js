a3545c1c3ac018714d43c18eafb4964d231b1da6
AliHMohammad <alihmohammad@hotmail.com>
2023-01-31 16:45:34 +0100

    6565776

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ document.querySelector("#OKKnap").addEventListener("click", sigHej);
 
 function sigHej() {
   let udfyldt = false;
+  let navn = document.querySelector("#navn").value;
   let alder = document.querySelector("#alder").value;
-  if (!document.querySelector("#navn").value) {
+  let email = document.querySelector("#email").value;
+
+  if (!navn) {
     document.querySelector("#resultat").textContent = "Navn er ugyldigt";
-  } else if (!alder || alder < 1) {
+  } else if (!alder || alder < 1 || alder > 100) {
     document.querySelector("#resultat").textContent = "Alder er ugyldigt";   
-  } else if (!document.querySelector("#email").value) {
+  } else if (!email) {
     document.querySelector("#resultat").textContent = "Email er ugyldigt"; 
   } else {
-    document.querySelector("#resultat").textContent = `Hej ${document.querySelector("#navn").value}! Du er ${document.querySelector("#alder").value} gammel, og din E-mail er ${document.querySelector("#email").value}`;
+    document.querySelector("#resultat").textContent = `Hej ${navn}! Du er ${alder} gammel, og din E-mail er ${email}`;
     udfyldt = true;
   }
 
   if (alder >= 30 && udfyldt) {
-    document.querySelector("#uAreOld").textContent = "Du er meget gammel!"
+    document.querySelector("#alderTekst").textContent = "Du er meget gammel!"
   } else if (alder <= 29 && alder > 0 && udfyldt) {
-    document.querySelector("#uAreOld").textContent = "Du er meget ung!"
+    document.querySelector("#alderTekst").textContent = "Du er meget ung!"
   } else {
-    document.querySelector("#uAreOld").textContent = null;
+    document.querySelector("#alderTekst").textContent = null;
   }
 }
