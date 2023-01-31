375405130387fc3ab92f9416b1124939988e22f8
AliHMohammad <alihmohammad@hotmail.com>
2023-01-31 12:08:28 +0100

    Ny ændring

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-document.querySelector("button").addEventListener("click", sayHello);
+document.getElementById("OKKnap").addEventListener("click", sayHello);
 
 function sayHello() {
-  document.querySelector("h3").textContent = `Hej ${document.querySelector("input").value}!`;
+  if (document.querySelector("input").value) {
+    document.querySelector("h3").textContent = `Hej ${document.querySelector("input").value}!`;
+  } else {
+    document.querySelector("h3").textContent = "Skriv venligst dit navn. Prøv igen";
+  }
 }
